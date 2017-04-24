87e3284addcaaabc1eb29dcd38082f222a0911fc
Bill Mill <bill@billmill.org>
2017-04-24 10:43:05 -0400

    it kind of works!

--- main.go
+++ main.go
@@ -2,14 +2,11 @@
 package main
 
 import (
-	"bufio"
 	"flag"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
-	"os"
-	"os/exec"
 	"time"
 
 	"github.com/gorilla/websocket"
@@ -37,48 +34,6 @@ const (
 	closeGracePeriod = 10 * time.Second
 )
 
-func pumpStdin(ws *websocket.Conn, w io.Writer) {
-	defer ws.Close()
-	ws.SetReadLimit(maxMessageSize)
-	ws.SetReadDeadline(time.Now().Add(pongWait))
-	ws.SetPongHandler(func(string) error { ws.SetReadDeadline(time.Now().Add(pongWait)); return nil })
-	for {
-		_, message, err := ws.ReadMessage()
-		if err != nil {
-			break
-		}
-		message = append(message, '\n')
-		if _, err := w.Write(message); err != nil {
-			break
-		}
-	}
-}
-
-func pumpStdout(ws *websocket.Conn, r io.Reader, done chan struct{}) {
-	defer func() {
-	}()
-	s := bufio.NewScanner(r)
-	for s.Scan() {
-		ws.SetWriteDeadline(time.Now().Add(writeWait))
-		log.Printf("writing %s", s.Bytes())
-		if err := ws.WriteMessage(websocket.TextMessage, s.Bytes()); err != nil {
-			log.Printf("error writing %s", err)
-			ws.Close()
-			break
-		}
-	}
-	if s.Err() != nil {
-		log.Println("scan:", s.Err())
-	}
-	close(done)
-
-	ws.SetWriteDeadline(time.Now().Add(writeWait))
-	log.Println("writing close message")
-	ws.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
-	time.Sleep(closeGracePeriod)
-	ws.Close()
-}
-
 func ping(ws *websocket.Conn, done chan struct{}) {
 	ticker := time.NewTicker(pingPeriod)
 	defer ticker.Stop()
@@ -102,6 +57,8 @@ func internalError(ws *websocket.Conn, msg string, err error) {
 
 var upgrader = websocket.Upgrader{}
 
+var commands = make(chan string)
+
 func serveWs(w http.ResponseWriter, r *http.Request) {
 	ws, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
@@ -111,61 +68,16 @@ func serveWs(w http.ResponseWriter, r *http.Request) {
 
 	defer ws.Close()
 
-	outr, outw, err := os.Pipe()
-	if err != nil {
-		internalError(ws, "stdout:", err)
-		return
-	}
-	defer outr.Close()
-	defer outw.Close()
-
-	inr, inw, err := os.Pipe()
-	if err != nil {
-		internalError(ws, "stdin:", err)
-		return
-	}
-	defer inr.Close()
-	defer inw.Close()
-
-	proc, err := os.StartProcess(cmdPath, flag.Args(), &os.ProcAttr{
-		Files: []*os.File{inr, outw, outw},
-	})
-	if err != nil {
-		internalError(ws, "start:", err)
-		return
-	}
-
-	inr.Close()
-	outw.Close()
-
-	log.Println("here")
-
-	stdoutDone := make(chan struct{})
-	go pumpStdout(ws, outr, stdoutDone)
-	go ping(ws, stdoutDone)
-
-	pumpStdin(ws, inw)
-
-	// Some commands will exit when stdin is closed.
-	inw.Close()
-
-	// Other commands need a bonk on the head.
-	if err := proc.Signal(os.Interrupt); err != nil {
-		log.Println("inter:", err)
-	}
-
-	select {
-	case <-stdoutDone:
-	case <-time.After(time.Second):
-		// A bigger bonk on the head.
-		if err := proc.Signal(os.Kill); err != nil {
-			log.Println("term:", err)
+	for {
+		cmd := <-commands
+		log.Printf("got cmd %s", cmd)
+		ws.SetWriteDeadline(time.Now().Add(writeWait))
+		log.Printf("writing %s", cmd)
+		if err := ws.WriteMessage(websocket.TextMessage, []byte(cmd)); err != nil {
+			log.Printf("error writing %s", err)
+			ws.Close()
+			break
 		}
-		<-stdoutDone
-	}
-
-	if _, err := proc.Wait(); err != nil {
-		log.Println("wait:", err)
 	}
 }
 
@@ -178,17 +90,27 @@ func serveHome(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, fmt.Sprintf("static%s", r.URL.Path))
 }
 
+func serveCommand(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, "Method not allowed", 405)
+		return
+	}
+
+	buf := make([]byte, 1024*1024)
+	num, err := r.Body.Read(buf)
+	if err != nil && err != io.EOF {
+		log.Printf("read error")
+		return
+	}
+
+	cmd := string(buf[:num])
+	log.Printf("read command %s", cmd)
+	commands <- cmd
+}
+
 func main() {
-	flag.Parse()
-	if len(flag.Args()) < 1 {
-		log.Fatal("must specify at least one argument")
-	}
-	var err error
-	cmdPath, err = exec.LookPath(flag.Args()[0])
-	if err != nil {
-		log.Fatal(err)
-	}
 	http.HandleFunc("/", serveHome)
+	http.HandleFunc("/command", serveCommand)
 	http.HandleFunc("/ws", serveWs)
 	log.Fatal(http.ListenAndServe(*addr, nil))
 }
